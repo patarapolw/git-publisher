aa7422721283c0b0af1c6bf7d31888c1dffdf93e
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-01-06 21:42:35 +0700

    fix tests

--- packages/make-html/tests/index.spec.ts
+++ packages/make-html/tests/index.spec.ts
@@ -1,17 +1,18 @@
 import glob from 'fast-glob'
 import fs from 'fs'
 import pkg from '../../../package.json'
-import MakeHtml from '../src'
+import MakeHtml from '@/.'
+import path from 'path'
 
 const makeHtml = new MakeHtml()
 
-describe('Parse with browser or JSDOM', () => {
+describe('Parse without browser or JSDOM', () => {
   glob.sync(`${pkg['git-publisher'].dir}/**/*.*`, {
     absolute: true,
     cwd: '../..',
   }).map((el) => {
     it(el, () => {
-      makeHtml.make(fs.readFileSync(el, 'utf8'))
+      makeHtml.make(fs.readFileSync(el, 'utf8'), path.extname(el))
     })
   })
 })
